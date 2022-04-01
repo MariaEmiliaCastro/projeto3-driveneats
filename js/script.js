5cf1980639af188fcf081e331d56954bb45800d6
MariaEmiliaCastro <emyly.mary@gmail.com>
2022-04-01 17:01:45 -0300

    Adição das seleçoes de itens

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
 function comidaSelecionada (comida) {
+
+    let comidaAnterior = document.querySelector('.prato-selecionado');
+
+    if (comidaAnterior !== null) {
+        comidaAnterior.classList.remove('prato-selecionado');
+    }
+
+    comida.classList.add("prato-selecionado");
+}
+
+function bebidaSelecionada (bebida) {
+
+    let bebidaAnterior = document.querySelector('.bebida-selecionada');
+
+    if (bebidaAnterior !== null) {
+        bebidaAnterior.classList.remove('bebida-selecionada');
+    }
+
+    bebida.classList.add("bebida-selecionada");
+}
+
+function sobremesaSelecionada (sobremesa) {
+
+    let sobremesaAnterior = document.querySelector('.sobremesa-selecionada');
+
+    if (sobremesaAnterior !== null) {
+        sobremesaAnterior.classList.remove('sobremesa-selecionada');
+    }
+
+    sobremesa.classList.add("sobremesa-selecionada");
+}
+
+function comprar (pedido) {
+
     
+
 }
